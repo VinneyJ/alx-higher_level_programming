d321793dfb70cd41f423f9d540a5d97412f9eba5
root <vincentjayden49@gmail.com>
2021-05-06 15:28:20 +0000

    eplaces all occurrences of an element by another in a new list

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = my_list[:]
+    for i in new_list:
+        idx = new_list.index(i)
+        if idx == search:
+            new_list[idx] = replace
+    return new_list
