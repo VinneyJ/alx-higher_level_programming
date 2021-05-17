4031495cd91f974328afa9991ee9e4f69796d12d
root <vincentjayden49@gmail.com>
2021-05-17 18:13:31 +0000

    prints x elements of a list.

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,9 +3,9 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     for n in range(x):
         try:
-            print("{}".format(my_list[n]), end=" ")
+            print("{}".format(my_list[n]), end="")
             count += 1
         except:
             break
-    print(" ")
+    print("")
     return count
