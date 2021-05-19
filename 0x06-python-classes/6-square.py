1545ad6357235917c25138cc75df65a0aa7161f6
root <vincentjayden49@gmail.com>
2021-05-19 20:48:54 +0000

    Square that defines a square by: (based on 5-square.py)

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Square that defines a square by: (based on 5-square.py)"""
+
+
+class Square:
+    """instance attribute: size and position"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if (
+                not isinstance(value, tuple) or
+                len(value) is not 2 or
+                type(value[0]) is not int or
+                type(value[1]) is not int or
+                value[0] < 0 or
+                value[1] < 0
+        ):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+            return
+        [print("") for n in range(self.__position[1])]
+        for n in range(self.__size):
+            print("{}{}".format(self.__position[0] * " ",
+                                self.__size * "#"), end="")
+            print()
