6309efc1fe7da7e68267e6d259561b009ce68ce1
root <vincentjayden49@gmail.com>
2021-05-19 20:42:38 +0000

    that prints in stdout the square with the character #

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Square that defines a square by: (based on 2-square.py)"""
+
+
+class Square:
+    """Private instance attribute: size"""
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            "size must be an integer, otherwise raise a TypeError"
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            "size must be more than zero"
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        for x in range(self.__size):
+            for i in range(self.size):
+                print("#", end="")
+            print("")
+        if self.__size == 0:
+            print("")
