def6476aca97367ddffc1ca86f4a33e8c41f190c
root <vincentjayden49@gmail.com>
2021-06-02 19:42:39 +0000

    updates

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
 """
-Contains definition of the empty class BaseGeometry
+Contains definition of class Rectangle that inherits from BaseGeometry.
 """
 
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-class BaseGeometry():
-    """Definition of class BaseGeometry"""
+
+class Rectangle(BaseGeometry):
+    """Definition of class Rectangle that inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
+        """Initialize an instance of class Rectangle"""
+
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__height = height
+        self.__width = width
 
     def area(self):
-        """Raise an Exception with message 'area() is not implemented'"""
         raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validates 'value' to be an integer greater than 0.
-        Args:
-            name (str): name of the value
-            value (unknown): to be validated
-        Raises:
-            TypeError - if value is not an integer
-            ValueErrror - if value is <= 0
-        """
-
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
