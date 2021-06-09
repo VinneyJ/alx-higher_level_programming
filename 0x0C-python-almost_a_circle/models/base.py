63720d654938ef44c7c7e7ca9d4ebc7da58a2cd2
root <vincentjayden49@gmail.com>
2021-06-09 12:00:41 +0000

    base update pep8

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 
+
 class Base:
+    """This is the base class"""
     __nb_objects = 0
+
     def __init__(self, id=None):
+
         if id is not None:
             self.id = id
         else:
-            __class__.__nb_objects+=1
+            __class__.__nb_objects += 1
             self.id = __class__.__nb_objects
