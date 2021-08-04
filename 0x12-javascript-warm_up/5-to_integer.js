177233560c0a92c1938dd123f4580e3427750e4f
root <vincentjayden49@gmail.com>
2021-08-04 12:26:58 +0000

    update convert to int

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
+const { argv } = require('process');
 
-const num = parseInt(process.argv[2]);
+const num = parseInt(argv[2]);
 if (num) {
   console.log('My number: ' + num);
 } else {
   console.log('Not a number');
 }
+
