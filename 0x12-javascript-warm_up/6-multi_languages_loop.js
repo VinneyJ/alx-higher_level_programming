f98307220f08c0413f96471f99d0284309923b7b
root <vincentjayden49@gmail.com>
2021-08-04 12:38:27 +0000

    update loop languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,13 @@
 #!/usr/bin/node
 
 const myList = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+myList.forEach((item) => {
+  console.log(item);
+});
+
+/*
+const myList = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (const i in myList) {
   console.log(myList[i]);
 }
+*/
