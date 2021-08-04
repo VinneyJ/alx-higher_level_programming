29fcf801e6214ef8efeeaf24109e28319741ef86
root <vincentjayden49@gmail.com>
2021-08-04 14:12:43 +0000

    add 2 numbers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,12 +1,39 @@
 #!/usr/bin/node
+const { argv } = require('process');
 
-const a = parseInt(process.argv[2]);
-const b = parseInt(process.argv[3]);
+/*
+//OPTION 1
+
+const arg1 = parseInt(argv[2]);
+const arg2 = parseInt(argv[3]);
 function add (a, b) {
-  if (a && b) {
-    console.log(a + b);
+  if (arg1 && arg2) {
+    console.log(arg1 + arg2);
   } else {
     console.log('NaN');
   }
 }
 add(a, b);
+*/
+
+/*
+  OPTION 2
+*/
+
+function add () {
+  if (argv.length === 4) {
+    const arg1 = parseInt(argv[2]);
+    const arg2 = parseInt(argv[3]);
+    let result = 0;
+    if (arg1 && arg2) {
+      argv.forEach((item) => {
+        result = (arg1 + arg2);
+      });
+      console.log(result);
+    }
+  } else {
+    console.log('NaN');
+  }
+}
+
+add();
