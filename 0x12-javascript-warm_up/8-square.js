8e1e0b8c3654710ace1aee2e50624d7e98e5d395
root <vincentjayden49@gmail.com>
2021-08-04 13:25:28 +0000

    prints a square size

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
 
+const { argv } = require('process');
+/*
+//Option 1
 const arg = parseInt(process.argv[2]);
 let inLine = '';
 let num = 0;
@@ -12,6 +15,28 @@ if (process.argv.length === 3 && arg) {
   while (numnum < arg) {
     console.log(inLine);
     numnum++;
+  }
+} else {
+  console.log('Missing size');
+}
+*/
+
+/*
+  Option 2
+*/
+const arg = parseInt(argv[2]);
+let string = '';
+let counter = 0;
+
+if (argv.length === 3 && arg) {
+  while (counter < arg) {
+    string += 'x';
+    counter++;
+  }
+  let secondCounter = 0;
+  while (secondCounter < counter) {
+    console.log(string);
+    secondCounter++;
   }
 } else {
   console.log('Missing size');
