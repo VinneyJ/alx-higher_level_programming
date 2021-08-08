4b876f269308bfc99010bdc0e43d8fccb77ee43c
root <vincentjayden49@gmail.com>
2021-08-08 23:45:03 +0000

    states objects contain later a

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Start linking class to table in database
+list states with "a"
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    sesh = sessionmaker(bind=engine)
+    session = sesh()
+
+    for data in session.query(State):
+        if 'a' in data.name:
+            print("{}: {}".format(data.id, data.name))
