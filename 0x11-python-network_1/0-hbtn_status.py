4df528c66d760c35ef9bd20d9efb21f817eb59b7
root <vincentjayden49@gmail.com>
2021-08-24 23:38:06 +0000

    update header response value

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,11 +3,12 @@
 """ It fetches Url """
 import urllib.request
 
-url = "https://intranet.hbtn.io/status"
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
 
-with urllib.request.urlopen(url) as response:
-    html = response.read()
-print("Body response:")
-print("\t- type:", type(html))
-print("\t- content:", html)
-print("\t- utf8 content:", html.decode("utf-8"))
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type:", type(html))
+        print("\t- content:", html)
+        print("\t- utf8 content:", html.decode("utf-8"))
