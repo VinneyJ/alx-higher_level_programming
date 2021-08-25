c046d9ec2e6a5d7f4b18eb1c6dfb0c91f02d2e8a
root <vincentjayden49@gmail.com>
2021-08-25 00:03:42 +0000

    sends a request to the URL and displays the value of the X-Request-Id

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,5 +6,5 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     with urllib.request.urlopen(url) as response:
-        var = response.info()
-        print(var.get("X-Request-Id"))
+        
+        print('{}'.format(response.info().get("X-Request-Id")))
