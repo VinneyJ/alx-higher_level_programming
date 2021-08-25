3f01a11b8e367c60f5ac271f3300cdabdae4ed2f
VinneyJ <vincentjayden49@gmail.com>
2021-08-25 03:59:58 +0300

    update json api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,20 +5,33 @@ request to http://0.0.0.0:5000/search_user
 with the letter as a parameter.
 """
 
-
 if __name__ == "__main__":
     import requests
     import sys
 
-    url = "http://0.0.0.0:5000/search_user"
-    arg1 = str(sys.argv[1])
-    data = {"q": ""}
-    if len(sys.argv) < 1:
-        print("No result")
-    else:
-        data["q"] = arg1
-        req = requests.post(url, data=data)
-        try:
-            print("[{:d}] {}".format(req.json()["id"], req.json()["name"]))
-        except Exception:
-            print("Not a valid JSON")
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
+    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+    try:
+        dic = r.json()
+        if dic == {}:
+            print('No result')
+        else:
+            print("[{}] {}".format(dic.get('id'), dic.get('name')))
+    except ValueError:
+        print('Not a valid JSON')
+# if __name__ == "__main__":
+#     import requests
+#     import sys
+
+#     url = "http://0.0.0.0:5000/search_user"
+#     arg1 = str(sys.argv[1])
+#     data = {"q": ""}
+#     if len(sys.argv) < 1:
+#         print("No result")
+#     else:
+#         data["q"] = arg1
+#         req = requests.post(url, data=data)
+#         try:
+#             print("[{:d}] {}".format(req.json()["id"], req.json()["name"]))
+#         except Exception:
+#             print("Not a valid JSON")
