577808b22188f86c46bfceb0fd581fb5a339223a
VinneyJ <vincentjayden49@gmail.com>
2021-08-25 03:42:24 +0300

    update post email

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,9 +1,16 @@
-!/usr/bin/python3
-import requests
-import sys
+#!/usr/bin/python3
+"""
+takes in a URL and an email address,
+sends a POST request to the passed URL with
+the email as a parameter, and finally displays
+the body of the response.
+"""
 
 
 if __name__ == "__main__":
+    import requests
+    import sys
+
     arg1 = sys.argv[1]
     arg2 = sys.argv[2]
     data = {}
