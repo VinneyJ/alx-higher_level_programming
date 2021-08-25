1f9fe6bf19df2d62ba12f960edfa26d334a42062
VinneyJ <vincentjayden49@gmail.com>
2021-08-25 03:47:12 +0300

    update json api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-import requests
-import sys
+"""
+takes in a letter and sends a POST
+request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+"""
 
 
 if __name__ == "__main__":
+    import requests
+    import sys
+
     url = "http://0.0.0.0:5000/search_user"
     arg1 = str(sys.argv[1])
     data = {"q": ""}
