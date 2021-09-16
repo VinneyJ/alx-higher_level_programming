5ef2cfdd1f5377632199637ca402afa31396fbcb
root <vincentjayden49@gmail.com>
2021-09-16 00:02:01 +0000

    movies where the Wedgeis present Antillescharacter

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const results = JSON.parse(body).results;
+    let x = 0;
+    let count = 0;
+    while (x < results.length) {
+      const dicts = results[x];
+      const characters = dicts.characters;
+      x++;
+      let y = 0;
+      while (y < characters.length) {
+        if (characters[y].match(/18/)) {
+          count += 1;
+        }
+        y++;
+      }
+    }
+    console.log(count);
+  }
+});
