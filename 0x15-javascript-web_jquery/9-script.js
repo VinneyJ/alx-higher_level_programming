a386f76b8565ba7665f277e8cde57e477be607f1
root <vincentjayden49@gmail.com>
2021-10-01 20:30:21 +0000

    say hello

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,11 @@
+$(function () {
+  const hello = $("#hello");
+  $.ajax({
+    type: 'GET',
+    url: 'https://fourtonfish.com/hellosalut/?lang=fr',
+    success: function (data){
+      hello.text(data.hello);
+    }
+  });
+  
+});
